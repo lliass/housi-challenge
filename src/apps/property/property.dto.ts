372b05aa4246f306adddf15b0a291ca67aee3cb5
Elias Lopes <elias.lopes@mevosaude.com.br>
2024-02-06 16:21:28 -0300

    feat(Property): Add tests

--- src/apps/property/property.dto.ts
+++ src/apps/property/property.dto.ts
@@ -13,7 +13,7 @@ class BusyDatesQueryRequestDTO {
   @Matches(/^\d{4}(-)(((0)[0-9])|((1)[0-2]))(-)([0-2][0-9]|(3)[0-1])$/i, {
     message: '$property must be formatted as yyyy-mm-dd',
   })
-  start: string;
+  start?: string;
 
   @IsOptional()
   @IsNotEmpty()
@@ -21,7 +21,7 @@ class BusyDatesQueryRequestDTO {
   @Matches(/^\d{4}(-)(((0)[0-9])|((1)[0-2]))(-)([0-2][0-9]|(3)[0-1])$/i, {
     message: '$property must be formatted as yyyy-mm-dd',
   })
-  end: string;
+  end?: string;
 }
 
 class BusyDatesParamRequestDTO {
