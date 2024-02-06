49e9bbc86bd0186e1afeb3beccafc2af101a2f8e
Elias Lopes <elias.lopes@mevosaude.com.br>
2024-02-06 10:47:04 -0300

    feat(Property): Add basic validators

--- src/apps/property/property.service.ts
+++ src/apps/property/property.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import {
+  BusyDatesParamRequestDTO,
+  BusyDatesQueryRequestDTO,
+} from './property.dto';
 
 @Injectable()
-export class PropertyService {}
+export class PropertyService {
+  async getBusyDates(
+    param: BusyDatesParamRequestDTO,
+    query: BusyDatesQueryRequestDTO,
+  ): Promise<string[]> {
+    return [];
+  }
+}
